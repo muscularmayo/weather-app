5acf2bf474c4a4cb4010303e26ea356845454c99
Chris Risden <christopherrisden@gmail.com>
2021-12-30 16:16:48 -0800

    current weather + gif results come in clearly, remains to be seen if i want to really make the front end look nice or just keep it as a simple ui with a joke concept

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ const img = document.querySelector('img'); //selecting the image so we can trans
 const noKeyNoLocationWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q='
 const noSearchGiphyURL = `https://api.giphy.com/v1/gifs/translate?api_key=${giphyAPIKey}&s=`
 
+appropriateGIF('search weather here')
 
 
 
-
-function searchCurrentWeather () {
-  const locationQuery = document.querySelector('#search').value;
-  const fetchURL = noKeyNoLocationWeatherURL + locationQuery + `&appid=${weatherAPIKey}`
+function searchCurrentWeather (location) {
+  const fetchURL = noKeyNoLocationWeatherURL + location + `&units=imperial&appid=${weatherAPIKey}`
   console.log(fetchURL)
   fetch (fetchURL)
     .then(function(response) {
@@ -23,28 +22,78 @@ function searchCurrentWeather () {
     })
     .then((data) => {
       console.log(data)
-      appropriateGIF(data.weather[0].description)
+      if (data.cod === '404') {
+        appropriateGIF('404 error')
+        console.log('location not found')
+      }
+      appropriateGIF(data.weather[0].description) //description of the weather
+      console.log(data.weather[0])
+      createCurrentWeatherDiv(data)
+      return data;
 
     })
-    .catch((err) => console.error(err))
+    .catch((err) => {
+      console.error(err)
+
+    })
 }
 
 function appropriateGIF (keyword) {
   const giphySearch = noSearchGiphyURL + keyword
-  console.log(keyword)
   fetch(giphySearch)
     .then(function(response) {
-      return response.json()
+      return response.json();
     })
     .then(function(response) {
       img.src = response.data.images.original.url
+      img.title = keyword;
     })
 }
 
 function handleWeatherSearch () {
-  searchCurrentWeather();
+  const locationQuery = document.querySelector('#search').value;
+  searchCurrentWeather(locationQuery);
+
+
+
 
 }
+
+function createCurrentWeatherDiv (currentWeatherData) {
+  clearDiv('#current')
+  const img = document.createElement('img')
+  const currentDiv = document.querySelector('#current')
+
+  const icon = currentWeatherData.weather[0].icon;
+  const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+  fetch (iconURL)
+    .then((response) => {
+      return response
+    })
+    .then((data) => {
+      img.src = data.url
+      currentDiv.appendChild(img)
+    })
+    .catch((err) => {
+      console.error(err)
+
+    })
+    const p = document.createElement('p')
+    p.innerText = `it is currently ${currentWeatherData.main.temp} degrees Farenheit in ${currentWeatherData.name}, and it feels like ${currentWeatherData.main.feels_like}. The 24-hour high is ${currentWeatherData.main.temp_max} Farenheit and the low is ${currentWeatherData.main.temp_min} Farenheit.`
+    currentDiv.appendChild(p);
+}
+
+function clearDiv (id) {
+  var e = document.querySelector(id);
+
+  //e.firstElementChild can be used.
+  var child = e.lastElementChild;
+  while (child) {
+      e.removeChild(child);
+      child = e.lastElementChild;
+  }
+}
+
 
 
 
