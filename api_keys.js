f6bd3ed9364808f74c278d000c2ef8634767b911
Chris Risden <christopherrisden@gmail.com>
2021-12-30 19:36:38 -0800

    adding api keys to this so we can try to host the page

---
+++ api_keys.js
@@ -0,0 +1,3 @@
+export const weatherAPIKey = '018c706ee5c7dea5e2a063440313acff';
+export const giphyAPIKey = 'jpg0bN4d11zxLuU5P37dXPbaXH2t10BT';
+export const pexelsAPIKey = '563492ad6f917000010000010f11ed22553642de8adea81a8de86aa8'
